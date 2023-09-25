6f234ed2f826c7e6ac503d0976958b4e2fc087ba
DanendraPassadhi <danendra.passadhi71@gmail.com>
2023-09-25 20:36:24 +0700

    Modifikasi menjadi nilai huruf

--- Jobsheet5/PemilihanPercobaan2_06.java
+++ Jobsheet5/PemilihanPercobaan2_06.java
@@ -16,8 +16,24 @@ public class PemilihanPercobaan2_06 {
         float tugas = input00.nextFloat();
 
         float total = (uas * 0.4f) + (uts * 0.3f) + (kuis * 0.1f) + (tugas * 0.2f);
-        String message = total < 65 ? "Remidi" : "Tidak remidi";
+        String nilai;
+        if (total>80 && total<=100 ) {
+            nilai = "A";
+          } else if (total> 73 && total<= 80) {
+            nilai = "B+";
+            } else if (total> 65 && total<= 73) {
+            nilai = "B";
+            } else if (total> 60 && total<= 65) {
+            nilai = "C+";
+            } else if (total> 50 && total<= 60) {
+            nilai = "C";
+            } else if (total> 39 && total<= 50) {
+            nilai = "D";
+            } else {
+            nilai = "E";
+          }
+      
         
-        System.out.println("Nilai akhir = " + total + " sehingga " + message);
+        System.out.println("Nilai akhir = " + total + " sehingga nilai anda " + nilai);
     }
 }
