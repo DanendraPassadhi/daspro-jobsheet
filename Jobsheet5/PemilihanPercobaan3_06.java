f3a2bcc41914ca5150373239a35503d8168effad
DanendraPassadhi <danendra.passadhi71@gmail.com>
2023-09-26 21:19:35 +0700

    menghilangkan break pada case 1

--- Jobsheet5/PemilihanPercobaan3_06.java
+++ Jobsheet5/PemilihanPercobaan3_06.java
@@ -19,14 +19,15 @@ public class PemilihanPercobaan3_06 {
         switch (operator) {
             case '+':
                 hasil = angka1 + angka2;
-                System.out.println(angka1+ " + " +angka2+ "=" +hasil);
-                break;
+                System.out.println(angka1+ " + " +angka2+ "=" +hasil);  
             case '-':
                 hasil = angka1 - angka2;
                 System.out.println(angka1+ " - " +angka2+ "=" +hasil);
+                break;
             case '*':
                 hasil = angka1 * angka2;
                 System.out.println(angka1+ " * " +angka2+ "=" +hasil);
+                break;
             case '/':
                 hasil = angka1 / angka2;
                 System.out.println(angka1+ " / " +angka2+ "=" +hasil);
