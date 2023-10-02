e796f0f27d58e16ccd2b2e12c6fc991fe41365f7
DanendraPassadhi <danendra.passadhi71@gmail.com>
2023-10-02 12:48:50 +0700

    Jobsheet6-Percobaan1

---
+++ Jobsheet6/Pemilihan2Percobaan1_06.java
@@ -0,0 +1,21 @@
+package Jobsheet6;
+
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan1_06 {
+    public static void main(String[] args) {
+        Scanner input06 = new Scanner(System.in);
+
+        int tahun;
+
+        System.out.println("Masukkan tahun : " );
+        tahun = input06.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) !=0)
+            System.out.println("Tahun kabisat");
+        } else {
+            System.out.println("Bukan tahun kabisat");
+        }
+    }    
+}
