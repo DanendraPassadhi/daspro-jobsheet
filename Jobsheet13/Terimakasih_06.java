0fc9dfaa9986246c9d9672c195d4fb93cd666f46
DanendraPassadhi <danendra.passadhi71@gmail.com>
2023-11-20 19:40:40 +0700

    Percobaan 1, Percobaan 2

--- Jobsheet13/Terimakasih_06.java
+++ Jobsheet13/Terimakasih_06.java
@@ -1,11 +1,16 @@
 package Jobsheet13;
 
-public class Terimakasih_06 {
+public class Terimakasih_06 { 
     public static void UcapanTerimaKasih() {
         System.out.println("Thank you for being the best teacher in the world.\n" + 
         "You inspired in me a love for learning and made me feel like I could ask you anyting.");
     }
+    public static void UcapanTambahan(String ucapan) {
+        System.out.println(ucapan);
+    }
     public static void main(String[] args) {
         UcapanTerimaKasih();
+        String ucapan = "Terima kasih Pak... Bu... Semoga Sehat Selalu";
+        UcapanTambahan(ucapan);
     }
 }
